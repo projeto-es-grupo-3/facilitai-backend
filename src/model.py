8dc6f369437c86c3dc8f66f1ef41933fd3b5bfa7
Pedro Serey <serey.pedro@gmail.com>
2023-04-19 23:23:58 -0300

    refactor: change models.py to model.py file

--- src/model.py
+++ src/model.py
@@ -8,7 +8,23 @@ class User(db.Model):
 
     username = db.Column(db.String(30), unique=True, nullable=False)
 
+    matricula = db.Column(db.String, unique=True, nullable=False)
+
+    pass_hash = db.Column(db.String, nullable=False)
+
     email = db.Column(db.String, unique=True, nullable=False)
+    
+    campus = db.Column(db.String, nullable=False)
+
+    curso = db.Column(db.String, nullable=False)
+
+    def __init__(self, username, email, matricula, campus, pass_hash, curso):
+        self.username = username
+        self.matricula = matricula
+        self.campus = campus
+        self.email = email
+        self.pass_hash = pass_hash
+        self.curso = curso
 
 
 class Anuncio(db.Model):
