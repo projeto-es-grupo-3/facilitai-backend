518b34ff4a4edaeb0c47a7f94475c337e0214ff4
VANDRESSAGALDINOS <vandressa.soares@ccc.ufcg.edu.br>
2023-04-23 16:22:19 -0300

    import abort

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, session
+from flask import Blueprint, request, jsonify, session, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from .model import (
