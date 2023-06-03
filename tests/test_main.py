c7cadfe87bf56255d931ad4743bb581f19ec0ff9
Pedro Serey <serey.pedro@gmail.com>
2023-06-03 01:02:51 -0300

    add: final test structure

---
+++ tests/test_main.py
@@ -0,0 +1,34 @@
+from model import User
+from config import (
+    REGISTER,
+    LOGIN,
+    LOGOUT,
+    CREATE_AD,
+    UPDATE,
+    DELETE_AD
+)
+
+
+def test_user_register_sucess(client, db_session):
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "username": "test-user-name",
+        "email": "test.email@gmai.com",
+        "matricula": "110110110",
+        "campus": "SEDE",
+        "password": "123456",
+        "curso": "CC"
+    }
+
+    print(dir(User))
+
+    response = client.post(REGISTER, headers=headers, json=body)
+
+    assert response.status_code == 201
+
+    user = db_session.query(User).first()
+    assert user.email == body['email']
+    assert user.username == body['username']
+
+    db_session.delete(user)
+    db_session.commit()
